fb6b87fa4a7447da414b97a8e863ad4547523642
Aayush Shrestha <119415261+Aayushstha10@users.noreply.github.com>
2026-07-01 10:25:28 +0545

    change

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import bookRoute from "./route/books.route.js";
 import cors from "cors";
+
+import bookRoute from "./route/books.route.js";
 import userRoute from "./route/user.route.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-const URI = process.env.MONGODB_URI; 
-console.log("uri",URI);
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(URI);
-    console.log("MongoDB connected successfully");
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(URI);
+      console.log("MongoDB connected successfully");
+    }
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
   }
 };
+
 connectDB();
+
+
+app.get("/", (req, res) => {
+  res.send("BookStore API is running...");
+});
 
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+export default app;
