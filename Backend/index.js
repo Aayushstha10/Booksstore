363e2f340c5c268a874f11851cf2ce01dca781f2
Aayush Shrestha <119415261+Aayushstha10@users.noreply.github.com>
2026-07-01 22:31:33 +0545

    Configure backend for Vercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import bookRoute from "./route/books.route.js";
 import cors from "cors";
+
+import bookRoute from "./route/books.route.js";
 import userRoute from "./route/user.route.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 4001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const URI = process.env.MONGODB_URI;
-console.log("uri", URI);
 
 const connectDB = async () => {
   try {
@@ -23,11 +24,18 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 connectDB();
 
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4001;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
