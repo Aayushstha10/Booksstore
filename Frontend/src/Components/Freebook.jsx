e7cf70446891f5f97351ae78dbc609bc64fb012a
Aayush Shrestha <119415261+Aayushstha10@users.noreply.github.com>
2026-07-02 21:01:54 +0545

    Configure backend for Vercel

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://booksstore-lime.vercel.app/book");
+        const res = await axios.get("http://localhost:4001/book");
         setBook(res.data.filter((data) => data.category === "free"));
       } catch (error) {
         console.log("Error:", error);
