fefb261c3b7a7dcfcb117f4b650387595230b9b2
Aayush Shrestha <119415261+Aayushstha10@users.noreply.github.com>
2026-07-03 10:43:59 +0545

    Configure backend for Vercel

--- Frontend/src/Components/Freebook.jsx
+++ Frontend/src/Components/Freebook.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import Cards from "./Cards";
-import API from "../../api"
+import API from "../../api";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
